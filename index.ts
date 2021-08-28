6c05ec5222ab3c9abbaaf328eb69dd7a1006887e
Anwesh43 <anweshthecool0@gmail.com>
2021-08-29 04:35:23 +0530

    adding SEFSNode class

--- index.ts
+++ index.ts
@@ -140,3 +140,46 @@ class Animator {
         }
     }
 }
+
+class SFESNode {
+
+    next : SFESNode 
+    prev : SFESNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SFESNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSFESNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SFESNode {
+        var curr : SFESNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
