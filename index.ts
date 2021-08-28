64f04c3bf6eb943f7e8c116745bc50822abbf86c
Anwesh43 <anweshthecool0@gmail.com>
2021-08-29 03:46:17 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -25,3 +25,42 @@ class ScaleUtil {
     }
 }
 
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSquareFromEitherSide(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            context.save()
+            context.translate((w / 2) * (1 - sc1), 0)
+            context.fillRect(0, -size / 2, size, size) 
+            context.restore()
+            context.save()
+            context.translate(size / 2, -(h / 2) * (1 - sc2) + (h / 2 + size) * sc3)
+            DrawingUtil.drawLine(context, 0, 0, 0, size)
+            context.restore()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSFESNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSquareFromEitherSide(context, scale)
+    }
+}
