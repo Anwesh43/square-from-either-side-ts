94e8d4bc187876252ec20b24860bc08a9d8813fc
Anwesh43 <anweshthecool0@gmail.com>
2021-08-29 04:01:58 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -84,7 +84,14 @@ class Stage {
 
     handleTap() {
         this.canvas.onmousedown = () => {
-            
+
         }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
